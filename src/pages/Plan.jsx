01ef1b438aea9b8fdc8924c4a15ea1fc8b738843
charlesncn <charowcjn@gmail.com>
2022-10-12 15:17:49 +0300

    bg img

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -6,28 +6,63 @@ const Plan = () => {
     {
       id: 1,
       name: 'Movie',
-      price: 1000,
+      price: 350,
       description: 'Movie in the morning as we wait for arrivals.',
-      img:'',
-    }
+      img:'https://github.com/charlesncn/stem/blob/main/src/imgs/movie-icon.png?raw=true',
+    },
+    {
+      id: 1,
+      name: 'Steam',
+      price: 1000,
+      description: 'Steam after the movie.',
+      img:'https://github.com/charlesncn/stem/blob/main/src/imgs/sauna.png?raw=true',
+    },
+    {
+      id: 1,
+      name: 'Food',
+      price: 300,
+      description: 'Food after the steam activity.',
+      img:'https://github.com/charlesncn/stem/blob/main/src/imgs/food.png?raw=true',
+    },
+    {
+      id: 1,
+      name: 'Kids Club',
+      price: 350,
+      description: 'Kids cub as they wait to be picked by their guardians.',
+      img:'https://github.com/charlesncn/stem/blob/main/src/imgs/kids-club.png?raw=true',
+    },
   ]
   return (
     <div className='plan'>
-      <div className="container">
         <h3>Choose Your Plan</h3>
-        <div className="right">
+      <div className="container">
+        <div className="left">
+          <h4>Weekdays</h4>
           {plans.map(plans => (
 
           <div className="card" key={plans.id}>
             <h4>{plans.name}</h4>
             <img src={plans.img} alt="" />
             <p>{plans.description}</p>
-            <span>{plans.price}</span>
+            <span>{plans.price}</span><br/>
             <Link to="" className="button">Book Now</Link>
           </div>
           ))}
         </div>
-        <div className="left"></div>
+        <div className="right">
+
+        <h4>Weekdends</h4>
+          {plans.map(plans => (
+
+          <div className="card" key={plans.id}>
+            <h4>{plans.name}</h4>
+            <img src={plans.img} alt="" />
+            <p>{plans.description}</p>
+            <span>{plans.price}</span><br/>
+            <Link to="" className="button">Book Now</Link>
+          </div>
+          ))}
+        </div>
       </div>
     </div>
   )
