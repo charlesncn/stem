98e9ade2cad518c6d4fec7fb5fd604df26a0c458
charlesncn <charowcjn@gmail.com>
2022-10-12 16:42:37 +0300

    Added media query 768

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,15 +14,70 @@ function Home() {
             Art, Math), Food, Movie & So Much More.
             <br />
             <br />
-            <br />
-            <br />
             You Also Get To Enjoy The Goodies From Our Sponsors.
           </p>
+          <span className="dates">Starting 26<sup>th</sup> & 27<sup>th</sup> November <br/> (onwards)</span>
           <br />
           <Link to="/plan" className="button">
             Book Now
           </Link>
         </div>
+      </div>
+      <div className="activities">
+        <h3>Activities</h3>
+        <div className="cardContainer">
+        <div className="card">
+          <span>Steam Activities</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/sauna.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+        <div className="card">
+          <span>Food</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/food.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+        <div className="card">
+          <span>Zumba</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/zumba.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+        <div className="card">
+          <span>Kids Club</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/kids-club.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+        <div className="card">
+          <span>Movie</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/movie-icon.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+        <div className="card">
+          <span>Origami</span>
+          <img
+            src="https://github.com/charlesncn/stem/blob/main/src/imgs/origami.png?raw=true"
+            className="gameName" alt=""
+          />
+
+        </div>
+
+
+        </div>
+
       </div>
       <div className="sponsors">
         <h3>Our Sponsors</h3>
