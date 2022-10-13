8fc6d8a78734496293766da2e642ffb7422c03bc
charlesncn <charowcjn@gmail.com>
2022-10-13 05:01:39 +0300

    Added sponsors back

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,8 +39,93 @@ function Home() {
           </div>
           <div className="left"></div>
         </div>
+
+      <div className="sponsors">
+        <h3>Potential Sponsors</h3>
+        <div className="cardContainer">
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/blueband.jpeg?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Blueband</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/bio-logo.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Bio Foods</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/ribena.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Ribena</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/Pizza_Hut.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Pizza Hut</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/kfc.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>KFC</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/nation-media.jpeg?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Nation Media Group</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/dtb.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>DTB</span>
+          </div>
+
+          <div className="card">
+            <img
+              src="https://github.com/charlesncn/stem/blob/main/src/imgs/kartasi.png?raw=true"
+              className="brandName"
+              alt=""
+            />
+
+            <span>Kartasi</span>
+          </div>
+        </div>
+      </div> 
       </div>
-      
+
 
       {/* <div className="activities">
         <h3>Activities</h3>
